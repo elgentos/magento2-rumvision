4fe2dd5aa45f1d4e86863f279b04059d879d1cfa
Jeroen Boersma <jeroen@elgentos.nl>
2023-03-03 11:50:35 +0100

    Added ConfigurationInterface

---
+++ src/Api/ConfigurationInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Elgentos\Rumvision\Api;
+
+interface ConfigurationInterface
+{
+    public const CONFIG_RUMVISION_ENALBED = 'elgentos_rumvision/general/enabled',
+                 CONFIG_RUMVISION_TRACKING_ID = 'elgentos_rumvision/general/tracking_id',
+                 CONFIG_RUMVISION_HOST_NAME = 'elgentos_rumvision/general/hostname';
+
+    public function isEnabled(int $storeId = null): bool;
+
+    public function getTrackingId(int $storeId = null) :string;
+
+    public function getHostName(int $storeId = null) :string;
+
+}
