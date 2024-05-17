dc7d03a68fe73d7b79cfcbe273b68619f84e4f71
Jeroen Boersma <jeroen@elgentos.nl>
2024-05-17 16:40:21 +0200

    Default to disable in developermode
    * fixes #5

--- src/Model/Config.php
+++ src/Model/Config.php
@@ -11,31 +11,46 @@ use Elgentos\Rumvision\Api\ConfigurationInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\App\State;
 
 class Config implements ConfigurationInterface
 {
-    /**
-     * @param ScopeConfigInterface $config
-     * @param StoreManagerInterface $storeManager
-     */
     public function __construct(
         private ScopeConfigInterface $config,
         private StoreManagerInterface $storeManager,
+        private State $appState,
     ) {}
 
     public function isEnabled(int $storeId = null): bool
     {
-        return (bool)$this->config->getValue(self::CONFIG_RUMVISION_ENALBED, ScopeInterface::SCOPE_STORE, $this->getStoreId($storeId));
+        if ($this->appState->getMode() === State::MODE_DEVELOPER) {
+            // Don't run in developer mode
+            return false;
+        }
+
+        return (bool)$this->config->getValue(
+            self::CONFIG_RUMVISION_ENABLED,
+            ScopeInterface::SCOPE_STORE,
+            $this->getStoreId($storeId)
+        );
     }
 
     public function getTrackingId(int $storeId = null): string
     {
-        return (string)$this->config->getValue(self::CONFIG_RUMVISION_TRACKING_ID, ScopeInterface::SCOPE_STORE, $this->getStoreId($storeId));
+        return (string)$this->config->getValue(
+            self::CONFIG_RUMVISION_TRACKING_ID,
+            ScopeInterface::SCOPE_STORE,
+            $this->getStoreId($storeId)
+        );
     }
 
     public function getHostName(int $storeId = null): string
     {
-        return (string)$this->config->getValue(self::CONFIG_RUMVISION_HOST_NAME, ScopeInterface::SCOPE_STORE, $this->getStoreId($storeId));
+        return (string)$this->config->getValue(
+            self::CONFIG_RUMVISION_HOST_NAME,
+            ScopeInterface::SCOPE_STORE,
+            $this->getStoreId($storeId)
+        );
     }
 
     public function getStoreId(int $storeId = null) :int
